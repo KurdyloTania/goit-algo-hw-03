826f86624acbc5a891ebc950e3fc7c505477227b
KurdyloTania <127951385+KurdyloTania@users.noreply.github.com>
2025-05-03 17:00:50 +0300

    Solved task without checking

---
+++ first_task.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        date_string = datetime.strptime(date, "%Y-%m-%d").date()
+        now = datetime.now().date()
+        days_difference = now - date_string
+        return days_difference
+    except ValueError:
+        print("Error! Please enter the date in the format \"YYYY-MM-DD\"!\n")
+        return None
+
+while True:      
+    user_input = input("Enter date in \"YYYY-MM-DD\" format: ")
+
+    count_days = get_days_from_today(user_input)
+    if count_days != None:
+        print(f"Number of days from a given date to the current one: {count_days.days}")
+        break
+        
---
+++ second_task.py
@@ -0,0 +1,40 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    has_error = False
+
+    if min < 1:
+        print("Minimum must be ≥ 1!")
+        has_error = True
+    
+    if max > 1000:
+        print("Maximum must be ≤ 1000!")
+        has_error = True
+    
+    if min > max:
+        print("Minimum must not exceed maximum!")
+        has_error = True
+    
+    if quantity < 1 or quantity > (max - min + 1):
+        print("Quantity must be within the valid range!")
+        has_error = True
+
+    if has_error:
+        print()
+        return None
+
+    return sorted(random.sample(range(min, max + 1), quantity))
+
+while True:
+    try:
+        min_number = int(input("Enter the minimum number (at least 1): "))
+        max_number = int(input("Enter the maximum number (no more than 1000): "))
+        quantity_number = int(input(f"Enter the quantity of numbers to pick (between {min_number} and {max_number}): "))
+       
+
+        lottery_numbers = get_numbers_ticket(min_number, max_number, quantity_number)
+        if lottery_numbers != None:
+            print(f"\nYour lottery numbers: {lottery_numbers}")
+            break
+    except ValueError:
+        print("Error! Please enter a number!\n")
